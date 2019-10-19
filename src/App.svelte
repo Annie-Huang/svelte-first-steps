eea859a7b0e26461dab10dc13a95c67712cfc86c
Annie-Huang <annie.hn@gmail.com>
2019-10-20 10:34:25 +1100

    Setup form fields

--- src/App.svelte
+++ src/App.svelte
@@ -1,12 +1,35 @@
 <script>
     import Product from "./Product.svelte";
-	export let name;
+
+    let title = '';
+    let price = 0;
+    let description = '';
 </script>
 
 <style>
-	h1 {
-		color: purple;
-	}
+    section {
+        width: 30rem;
+        margin: auto;
+    }
+
+    label, input, textarea {
+        width: 100%;
+    }
 </style>
 
-<Product />
+<section>
+    <div>
+        <label for="title">Title</label>
+        <input type="text" id="title"/>
+    </div>
+    <div>
+        <label for="price">Price</label>
+        <input type="number" id="price"/>
+    </div>
+    <div>
+        <label for="description">Description</label>
+        <textarea rows="3" id="description"></textarea>
+    </div>
+</section>
+
+<Product/>
