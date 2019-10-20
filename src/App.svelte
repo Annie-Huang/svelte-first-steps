03b87554a8284b9701beb835fbb63d71d8bb8099
Annie-Huang <annie.hn@gmail.com>
2019-10-20 17:10:53 +1100

    Add the bindings

--- src/App.svelte
+++ src/App.svelte
@@ -4,6 +4,10 @@
     let title = '';
     let price = 0;
     let description = '';
+
+    function setTitle(event) {
+        title = event.target.value;
+    }
 </script>
 
 <style>
@@ -17,19 +21,22 @@
     }
 </style>
 
+<!-- Can do on:click, on:blur, on:input        -->
+
+<p>{title}</p>
 <section>
     <div>
         <label for="title">Title</label>
-        <input type="text" id="title"/>
+        <input type="text" id="title" value="{title}" on:input="{setTitle}"/>
     </div>
     <div>
         <label for="price">Price</label>
-        <input type="number" id="price"/>
+        <input type="number" id="price" bind:value={price}/> <!-- svelte will convert the price into a number for us -->
     </div>
     <div>
         <label for="description">Description</label>
-        <textarea rows="3" id="description"></textarea>
+        <textarea rows="3" id="description" bind:value={description}></textarea>
     </div>
 </section>
 
-<Product/>
+<Product productTitle={title} productPrice={price} productDescription={description} />
