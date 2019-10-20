a08dab55c8e8f562b6ccb7ef21014f3d773242a8
Annie-Huang <annie.hn@gmail.com>
2019-10-20 18:49:03 +1100

    Calculate total

--- src/Cart.svelte
+++ src/Cart.svelte
@@ -1,5 +1,15 @@
 <script>
     export let items;
+
+    //$: is actually a regular Javascript syntax. It's called a labeled statement
+    // You typically don't use it. You could use it in conjunction with loops and to continue and break keywords
+    // Svelte hijacks it and make sure that whatever you write after this label will be re-executed by svelte
+    // whenever one of its input values changes
+    //
+    // My questions: Why didn't it need let in front of carTotal?
+    $: cartTotal = items.reduce((sum, curValue) => {
+        return sum + curValue.price;
+    }, 0);
 </script>
 
 <style>
@@ -21,10 +31,14 @@
     }
 </style>
 
-<ul>
-    {#each items as item}
-        <li>{item.title} - {item.price}</li>
-    {/each}
-</ul>
+{#if items.length === 0}
+    <p>No items in cart yet.</p>
+{:else}
+    <ul>
+        {#each items as item}
+            <li>{item.title} - {item.price}</li>
+        {/each}
+    </ul>
 
-<h1>TOTAL: $...</h1>
+    <h1>TOTAL: ${cartTotal}</h1>
+{/if}
